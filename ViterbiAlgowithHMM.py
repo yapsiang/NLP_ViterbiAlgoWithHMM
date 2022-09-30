fafe3c33c33ab09d90a8eeb7e8aef37b6ceca323
yapsiang <c190077@e.ntu.edu.sg>
2022-09-30 16:36:43 +0800

    update

--- ViterbiAlgowithHMM.py
+++ ViterbiAlgowithHMM.py
@@ -7,14 +7,14 @@ states = ["-Hot-", "-Cold-"]
 observations = ["1", "2", "3"]
 #array index:   0, 1, 2
 
-model = hmm.MultinomialHMM(n_components=2)
+model = hmm.CategoricalHMM(n_components=2)
 model.startprob_ = np.array([0.8, 0.2])
 model.transmat_ = np.array([[0.7, 0.3], 
                             [0.4, 0.6]])
 model.emissionprob_ = np.array([[0.2, 0.4, 0.4], [0.5, 0.4, 0.1]])
 
 obs_sqn_1 = np.atleast_2d([2, 0, 1, 2, 0, 1, 2, 0, 1]).T
-#print(model.decode(obs_sqn_1))
+# print(model.decode(obs_sqn_1))
 logprob, decode_states_1 = model.decode(obs_sqn_1, algorithm="viterbi")
 print ("Seq 1 decoded states: " + "".join(map(lambda x: states[x], decode_states_1)))
 
